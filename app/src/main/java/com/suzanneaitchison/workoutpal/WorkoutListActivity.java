f5a93ed838ea7fe2d0850cd8d79d58902bc72705
suzanne <suzanne.aitchison@gmail.com>
2018-03-06 20:48:14 +0000

    create empty menu drawer

--- app/src/main/java/com/suzanneaitchison/workoutpal/WorkoutListActivity.java
+++ app/src/main/java/com/suzanneaitchison/workoutpal/WorkoutListActivity.java
@@ -1,13 +1,43 @@
 package com.suzanneaitchison.workoutpal;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WorkoutListActivity extends AppCompatActivity {
+
+    @BindView(R.id.nav_drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_list);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
